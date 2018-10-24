f2d886c979aa3babdfd0bec81a723b41b35a7e69
willy7728 <43198451+willy7728@users.noreply.github.com>
2018-10-25 00:25:55 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String v1 = scn.next(), v2 = scn.next(), v3 = scn.next(), v4 = scn.next();
+        switch (v1){
+            case "Spring": System.out.println("2~4");
+                break;
+            case "Summer": System.out.println("5~7");
+                break;
+            case "Autumn": System.out.println("8~10");
+                break;
+            case "Winter": System.out.println("11~1");
+                break;
+        }
+        switch (v2){
+            case "Spring": System.out.println("2~4");
+                break;
+            case "Summer": System.out.println("5~7");
+                break;
+            case "Autumn": System.out.println("8~10");
+                break;
+            case "Winter": System.out.println("11~1");
+                break;
+        }
+        switch (v3){
+            case "Spring": System.out.println("2~4");
+                break;
+            case "Summer": System.out.println("5~7");
+                break;
+            case "Autumn": System.out.println("8~10");
+                break;
+            case "Winter": System.out.println("11~1");
+                break;
+        }
+        switch (v4){
+            case "Spring": System.out.println("2~4");
+                break;
+            case "Summer": System.out.println("5~7");
+                break;
+            case "Autumn": System.out.println("8~10");
+                break;
+            case "Winter": System.out.println("11~1");
+                break;
+        }
+    }
+}
